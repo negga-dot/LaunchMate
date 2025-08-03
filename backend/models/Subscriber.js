1771d25f014db892a5557d8575d30f414760fd0d
alpha9av <alpha9coder@gmail.com>
2025-08-03 05:52:15 +0530

    create subscriber model for newsletter signups

---
+++ backend/models/Subscriber.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const SubscriberSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  subscribedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Subscriber', SubscriberSchema);
